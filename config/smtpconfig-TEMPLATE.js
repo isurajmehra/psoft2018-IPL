af6a18e569bfa57d7f7db49f4b0da61b7aa0ba05
grjoshi <ever3stmomo@gmail.com>
2017-04-21 22:42:07 -0500

    Changed smtpconfig to support gmail address

--- config/smtpconfig-TEMPLATE.js
+++ config/smtpconfig-TEMPLATE.js
@@ -1,10 +1,23 @@
 /**
- * (C)grjoshi 6/8/2016
+ * (C)grjoshi 4/20/2016
  * smtpconfig.js - Nodemailer configuration object
- * First-run instructions: Rename this file to smtpconfig.js in the same folder
+ * First-run instructions: Rename this file to smtpconfig.js in the config/ folder
  //==============================================================================
  */
 
+
+//now supports gmail addresses
+
+module.exports = {
+    enabled: true,
+    service: 'gmail',
+    auth: {
+        user: '[gmail address]@gmail.com',
+        pass: '[gmail_password]'
+    }
+};
+//delete the above block and uncomment the following to use server's SMTP settings
+/*
 module.exports = {
     enabled: true,
     host: 'smtp_server_here',
@@ -12,3 +25,4 @@ module.exports = {
     user: 'user@email.com',
     password: '********'
 };
+*/
